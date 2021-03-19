a74068a0acff2e18188ea8e8c6dc6a7fe70e203d
Lucas V. Moog Brentano <brentano.l@gmail.com>
2021-03-19 20:29:29 -0300

    Update ex072.py

--- ex072.py
+++ ex072.py
@@ -2,6 +2,29 @@
 
 NUMBERS = ("zero", "um" , "dois", "três", "quatro", "cinco", "seis", "sete", "oito", "nove" , "dez")
 
-num = int(input("Digite um número entre 0-10:"))
+while True:
 
-print(f"Você digitou o número {NUMBERS[num]}.\n")
+    # verificador de dados digitados
+    while True:
+        choice = " "
+        num = int(input("Digite um número entre 0-10:"))
+        if num >= 0 and num <= 10:
+            break
+        print("Tente novamente.", end=" ")
+        
+
+    print(f"Você digitou o número {NUMBERS[num]}.\n")
+
+    #Verificador se a pessoa quer continuar
+    while choice not in "SN":
+        while True:
+            choice = str(input("Queres continuar? [s/n]").upper().strip()[0])
+            if choice != "N" and choice != "S":
+                print("Opção inválida, tente novamente.", end=" ")
+            else:
+                break
+
+    if choice == "N":
+        break
+    
+print("FIM")
