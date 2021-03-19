8ee18789a9c9d3a07dee1977c6ac2f76878e4bfe
Lucas V. Moog Brentano <brentano.l@gmail.com>
2021-03-19 20:38:17 -0300

    Update ex073.py

--- ex073.py
+++ ex073.py
@@ -3,8 +3,7 @@
 PLACEMENT = ("Maikon's", "Tigers", "Aquatic Pandas", "Xingrilhos", "Road Side Imps", "Mecha Banana", "Cotton Grenader", "Sweet Lamp")
 
 print(f" Os 5 primeiros colocados são: {PLACEMENT[:5]}")
-print(f"\n Os últimos 4 colocados são: {PLACEMENT[5:]}")
+print(f"\n Os últimos 4 colocados são: {PLACEMENT[-4:]}")
 print(f"\n Lista dos times em ordem alfabética: {sorted(PLACEMENT)}")
-
-#print(f"\n O time Aquatic Pandas está na posição {PLACEMENT.index("Aquatic Pandas")} da lista.")
+print(f"\n O time Aquatic Pandas está na posição {PLACEMENT.index('Aquatic Pandas')+1} da lista.\n")
 
