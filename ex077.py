2c1011477b33734de8ab9f68669d99aa804fb6e7
Lucas V. Moog Brentano <brentano.l@gmail.com>
2021-03-20 20:36:17 -0300

    Update ex077.py

--- ex077.py
+++ ex077.py
@@ -1,15 +1,25 @@
 #listador de vogais em tuplas
-
-words = ("Batata", "Milho", "Pineapple")
-
-print(f"Na palavra {words[0]} existe as seguintes vogais: ", end="")
+print()
+words = ("Batata", "Milho", "Pineapple", "Apfel", "Orange")
 
 
-for c in words:
-    if c.upper().count("A") >= 1:
+
+for c in range(0,len(words)):
+
+    print(f"Na palavra {words[c]} existe as seguintes vogais: ", end="")
+
+    
+    #Verificar de letras em cada palavra
+    if words[c].upper().count("A") >= 1:
         print("A", end=" ")
-    if c.upper().count("E") >= 1:
+    if words[c].upper().count("E") >= 1:
         print("E", end=" ")
-    if c.upper().count("I") >= 1:
+    if words[c].upper().count("I") >= 1:
         print("I", end =" ")
+    if words[c].upper().count("O") >= 1:
+        print("O", end=" ")
+    if words[c].upper().count("U") >= 1:
+        print("U", end="")
+    
+    print("\n")
 
