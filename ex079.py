f0a202de80cfdc21535c101aa67f0fc5a1757fe7
Lucas V. Moog Brentano <brentano.l@gmail.com>
2021-04-08 12:41:54 -0300

    ex79.py

---
+++ ex079.py
@@ -0,0 +1,24 @@
+#Number inserter in list, don't insert if the number typed is repeated.
+
+numbers = []
+choice = "Y"
+
+#Want to continue verifier.
+while choice == "Y":
+
+    num = int(input("Type a number: "))
+
+    #Typed number verifier
+    if num not in numbers:
+        numbers.append(num)
+        print("Number added successful.")
+    else:
+        print("Number is already in the list.")
+
+    choice = input("Want to continue? [Y/N]").upper()
+
+print(f"\nThe numbers that you typed are: {numbers}")
+
+print("="*30)
+print("END".center(30))
+print("="*30)
