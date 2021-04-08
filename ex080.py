e6fa90a61e94a420749e020c9a351924a17094af
Lucas V. Moog Brentano <brentano.l@gmail.com>
2021-04-08 18:59:47 -0300

    Create ex080.py

---
+++ ex080.py
@@ -0,0 +1,25 @@
+#Inserting numbers in list and sorting it, without sort() method
+
+numbers = []
+
+for i in range(8):
+    
+    num = int(input("Type an number: "))
+
+    #Insert any number typed if the list has no numbers inside it.
+    if len(numbers) == 0:
+        numbers.insert(0, num)
+    else:
+        #Number Placement Verifier 
+        if num >= max(numbers):
+            numbers.insert(i+1, num)
+        elif num <= min(numbers):
+            numbers.insert(0, num)
+        else:
+            for c in range(len(numbers)):
+                if num < numbers[c] and num not in numbers:
+                    numbers.insert(c, num)
+
+print("="*30)
+print(numbers)
+
