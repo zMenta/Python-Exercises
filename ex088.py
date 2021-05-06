65917646bf88fca633d4839fc6d5fef51fcbc493
Lucas V. Moog Brentano <70714721+zMenta@users.noreply.github.com>
2021-05-06 16:49:58 -0300

    Create ex088.py

---
+++ ex088.py
@@ -0,0 +1,19 @@
+#gambler helper. Helps a gambler to come up with lucky numbers.
+
+from random import randint
+
+gambles = []
+
+for i in range(int(input("How many guesses you want to make?"))):
+    guess = []
+    for c in range(6): #generating guesses for the gamble
+        number = randint(1,60)
+        guess.append(number)
+    
+    gambles.append(guess[:])
+
+print("="*50)
+for i in range(len(gambles)):
+    print(f"Guess {i} : {gambles[i]}")
+
+print("="*50)
