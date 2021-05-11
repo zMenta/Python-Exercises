8b9970b0404ee6eff5276b3c91c3737c4a15eef9
Lucas V. Moog Brentano <70714721+zMenta@users.noreply.github.com>
2021-05-11 17:00:44 -0300

    Update ex089.py

--- ex089.py
+++ ex089.py
@@ -1,13 +1,10 @@
 #Create a school report card. That can register various students and 2 grades for each student.Show the students average grade and if you want to check a specific studen grade at the end of the algorithm. Using only 1 list to store all the data.
-
 report = [] 
 student = []
-
 # Report contains students
 # Each student list has, index [0] = Name | [1] = 1th grade | [2] = 2th grade | [3] =  grade average
 
-
-for i in range(3): #Register students for the report list.
+for i in range(2): #Register students for the report list.
     print("="*50)
     student.append(str(input("Please type the name of the student: ")))
     for c in range(1,3):
@@ -16,5 +13,9 @@ for i in range(3): #Register students for the report list.
     report.append(student[:])
     student.clear()
     
+print("="*50)
+for i in range(len(report)):
+    print(f"The student {report[i][0]} have an grade average of {report[i][3]}")
 
 print(report)
+
