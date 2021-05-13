d90c301e9444b201130ff851b44f95483120e2a6
Lucas V. Moog Brentano <70714721+zMenta@users.noreply.github.com>
2021-05-13 18:39:52 -0300

    Finished ex089.py

--- ex089.py
+++ ex089.py
@@ -1,32 +1,31 @@
 #Create a school report card. That can register various students and 2 grades for each student.Show the students average grade and if you want to check a specific student grade at the end of the algorithm. Using only 1 list to store all the data.
 
-report = [["Carlos", 10, 8 ,9.85], ["Rodrigo", 4, 5, 6.345], ["Gustavo", 3, 8, 7.781]]  #TEST
-#report = []
+report = []
 student = []
 
 # Report contains students
 # Each student list has, index [0] = Name | [1] = 1th grade | [2] = 2th grade | [3] =  grade average
 
-# while True: #Register students for the report list.
-#     print("="*50)
-#     student.append(str(input("Please type the name of the student: ")).strip())
-#     for c in range(1,3):
-#         student.append(float(input(f"Please type {student[0]}'s {c}th grade: ")))
-#     student.append((student[1]+student[2])/2) #add the grade average of this student 
-#     report.append(student[:])
-#     student.clear()
+while True: #Register students for the report list.
+    print("="*50)
+    student.append(str(input("Please type the name of the student: ")).strip())
+    for c in range(1,3):
+        student.append(float(input(f"Please type {student[0]}'s {c}th grade: ")))
+    student.append((student[1]+student[2])/2) #add the grade average of this student 
+    report.append(student[:])
+    student.clear()
 
-#     while True: # Checks if the user wants to register more students
-#         print("-"*50)
-#         choice = str(input("Do you want to register another student? (Y/N)").upper())
+    while True: # Checks if the user wants to register more students
+        print("-"*50)
+        choice = str(input("Do you want to register another student? (Y/N)").upper())
         
-#         if choice != "Y" and choice != "N": #Checks if the answer is valid 
-#             print("Invalid answer, please try again.")
-#         else:
-#             break
+        if choice != "Y" and choice != "N": #Checks if the answer is valid 
+            print("Invalid answer, please try again.")
+        else:
+            break
     
-#     if choice == "N": #Stops asking for new students.
-#         break
+    if choice == "N": #Stops asking for new students.
+        break
 
 print("="*50)
 print("|{:^6}|{:^19}|{:^21}|".format("Num.","NAME","Final Grade"))
@@ -34,7 +33,6 @@ print("|{:6}|{:19}|{:21}|".format("-"*6, "-"*19, "-"*21 ))
 for i in range(len(report)):  #Shows the students final grades and names in a chart.
     print(f"|{i:^6}|{report[i][0]:^19}|{report[i][3]:^21.1f}|")
 print("="*50)
-print(report)
 
 while True: #Asks if the user want to check for specific students grades.
     while True:
@@ -48,8 +46,10 @@ while True: #Asks if the user want to check for specific students grades.
     if choice == "N": #Don't ask for a specific student grade if choice is "N"
         break
     else:
+        print("-"*50)
         study_name = str(input("Please type the name of the student: ")).strip()
         for i in range(len(report)):
+            name_found = False
             if report[i][0] == study_name:
                 print("="*50)
                 print("|{:^6}|{:^19}|{:^10}|{:^10}|".format("Num.","NAME", "Grade 1", "Grade 2" ))
@@ -58,8 +58,7 @@ while True: #Asks if the user want to check for specific students grades.
                 print("="*50)
                 name_found = True
                 break
-        if name_found not True:
-
-        
-
-
+        if name_found is not True:
+            print("-"*50)
+            print("Student not found.")
+            print("-"*50)
