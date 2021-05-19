88fbaaa369610737a1d12e3df982acdad6c63e5f
Lucas Brentano <brentano.l@gmail.com>
2021-05-19 15:32:06 -0300

    "working/finished"
    It's working but I need to figure a way out how to make the loop stop.

--- MyExercises/ordernumbers-2.py
+++ MyExercises/ordernumbers-2.py
@@ -1,19 +1,22 @@
 #order numbers from any amount of numbers.
 
 number = []
+from time import sleep
+from random import randint
 
 
 qt = int(input("How many numbers you want to type?")) #qt = quantity 
 
 for i in range(1,qt+1):
-    number.append(int(input(f"Type the {i}th number: ")))
-print(number)
+    # number.append(int(input(f"Type the {i}th number: ")))
+    number.append(randint(0,100))
+while True:
 
-while number[0] > number[1]:
     for i in range(1,qt):
-        while number[i-1] > number[i]:
             if number[i-1] > number[i]:
                 number[i-1],number[i] = number[i], number[i-1]
-            
-    
+
+    print(number)
+    sleep(0.6)
+  
 print(number)
