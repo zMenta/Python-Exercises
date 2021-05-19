b7adccfc6f2ca4b81936ec6a51a7b93999cc9c91
Lucas Brentano <brentano.l@gmail.com>
2021-05-19 15:12:59 -0300

    Trying

--- MyExercises/ordernumbers-2.py
+++ MyExercises/ordernumbers-2.py
@@ -1,5 +1,19 @@
 #order numbers from any amount of numbers.
 
-numbers = []
-ch = "s"
+number = []
 
+
+qt = int(input("How many numbers you want to type?")) #qt = quantity 
+
+for i in range(1,qt+1):
+    number.append(int(input(f"Type the {i}th number: ")))
+print(number)
+
+while number[0] > number[1]:
+    for i in range(1,qt):
+        while number[i-1] > number[i]:
+            if number[i-1] > number[i]:
+                number[i-1],number[i] = number[i], number[i-1]
+            
+    
+print(number)
