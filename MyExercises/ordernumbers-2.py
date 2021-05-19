2317e4a67b6b3d54e6986ad00de0dd005e12b42d
Lucas Brentano <brentano.l@gmail.com>
2021-05-19 17:33:30 -0300

    Update ordernumbers-2.py

--- MyExercises/ordernumbers-2.py
+++ MyExercises/ordernumbers-2.py
@@ -1,15 +1,15 @@
 #order numbers from any amount of numbers.
 
-number = []
 from time import sleep
 from random import randint
 
+number = []
 
 qt = int(input("How many numbers you want to type?")) #qt = quantity 
 
 for i in range(1,qt+1):
     # number.append(int(input(f"Type the {i}th number: ")))
-    number.append(randint(0,100))
+    number.append(randint(0,500))
 while True:
 
     for i in range(1,qt):
@@ -17,6 +17,6 @@ while True:
                 number[i-1],number[i] = number[i], number[i-1]
 
     print(number)
-    sleep(0.6)
+    sleep(0.4)
   
 print(number)
