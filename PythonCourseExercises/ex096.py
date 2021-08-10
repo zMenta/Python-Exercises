3088ea55dce3fcd91c6f4274b225478169b67dca
Lucas Brentano <brentano.l@gmail.com>
2021-08-10 10:50:59 -0300

    Update ex096.py

--- PythonCourseExercises/ex096.py
+++ PythonCourseExercises/ex096.py
@@ -3,8 +3,7 @@
 #Area function
 def area(a,b):
     print("-"*30)
-    print(f"The area of {a}x{b} is {a*b}")
+    print(f"The area of {a}x{b} is {a*b}m²")
     print("-"*30)
-
 
 area(int(input("Type height a: ")), int(input("Type width b: ")))
