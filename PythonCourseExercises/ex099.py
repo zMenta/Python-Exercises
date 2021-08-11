70860ccfb731323ebe32035a15a5ae2c502dc8a5
Lucas Brentano <brentano.l@gmail.com>
2021-08-11 13:13:00 -0300

    ex099.py Improvements

--- PythonCourseExercises/ex099.py
+++ PythonCourseExercises/ex099.py
@@ -11,14 +11,16 @@ def big(*num):
 
 def big2(*num): #Max number without the max() function
     line()
-    print(f"From the number(s) {num} the biggest is: ",end="")
-    for i,n in enumerate(num): #i = index | n = number
-        if i == 0:
-            #m = max
-            m = n
-        elif n > m:
-            m = n
-    print(m)
+    if len(num) == 0:
+        print(f"No numbers were typed.")
+    else:
+        for i,n in enumerate(num): #i = index | n = number
+            if i == 0:
+                #m = max
+                m = n
+            elif n > m:
+                m = n
+        print(f"From the number(s) {num} the biggest is: {m}")
 
 #Main Body
 
@@ -27,5 +29,11 @@ big2(2,4,5,8)
 big2(0,8,2,152,6)
 
 big2(6,2,9)
+
+big2(0)
+
+big2(6)
+
+big2()
 
 line()
