f3e02fd0c720c38e0f8dbacdd06c54557bda91b0
Lucas Brentano <brentano.l@gmail.com>
2021-08-12 11:02:44 -0300

    Update ex103.py

--- PythonCourseExercises/ex103.py
+++ PythonCourseExercises/ex103.py
@@ -7,10 +7,15 @@ def form(p="<unkown>",g=0):
 
     Both optional 
     """
+    if p == "":
+        p = "<unkown>"
+    if g == "":
+        g = 0
     print(f"Players name: {p} | Goals: {g}")
 
 
 #main body
-form()
-form("Pablo",2)
-form(g=5)
+a = input("Type the players name: ")
+b = input("The amount of goals he/she made: ")
+
+form(a,b)
