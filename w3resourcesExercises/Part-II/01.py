8de64eb38387775917c8e8ea41d60a5bfe603b5e
LucasBrentano <brentano.l@gmail.com>
2022-09-14 16:37:41 -0300

    w3 | ex01

---
+++ w3resourcesExercises/Part-II/01.py
@@ -0,0 +1,12 @@
+# Write a Python function that takes a sequence of numbers and determines whether all the numbers are different from each other.
+
+def verify_number(number: int):
+    string_number = str(number)
+
+    for i in string_number:
+        print(i)
+#        for i in len(number):
+#            pass
+
+
+verify_number(148)
