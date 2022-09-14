baaa57e55febd96174475d9e51d9a6280ce4675c
Lucas Brentano <brentano.l@gmail.com>
2022-09-14 19:21:41 -0300

    w3 | ex01

--- w3resourcesExercises/Part-II/01.py
+++ w3resourcesExercises/Part-II/01.py
@@ -1,12 +1,26 @@
 # Write a Python function that takes a sequence of numbers and determines whether all the numbers are different from each other.
 
 def verify_number(number: int):
+    """Validates a number sequence if there are any number that repeat itself.
+
+    Args:
+        number (int): Number sequence to validate
+
+    Returns:
+        bool: Returns True if a number in the sequence repeat itself.
+    """
     string_number = str(number)
+    number_size = len(string_number)
+    counter = 0
 
-    for i in string_number:
-        print(i)
-#        for i in len(number):
-#            pass
+    for current_number in string_number:
+        counter += 1
+        for i in range(counter, number_size):
+            if string_number[i] == current_number:
+                return True
+            
+    return False
 
 
-verify_number(148)
+print(verify_number(1406908))
+print(verify_number(1956))
