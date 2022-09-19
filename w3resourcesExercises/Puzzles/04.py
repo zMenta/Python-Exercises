a7da0ef64c67c3371e551b89a8e4ca9e9a9109af
Lucas V. Moog Brentano <brentano.l@gmail.com>
2022-09-19 19:21:10 -0300

    Some tweaks

--- w3resourcesExercises/Puzzles/04.py
+++ w3resourcesExercises/Puzzles/04.py
@@ -1,6 +1,5 @@
 # We are making n stone piles! The first pile has n stones. If n is even, then all piles have an even number of stones. If n is odd, all piles have an odd number of stones. Each pile must more stones than the previous pile but as few as possible. Write a Python program to find the number of stones n each pile.#
 
-
 pile_size = int(input("Type stone pile amount: "))
 
 stone_pile = []
